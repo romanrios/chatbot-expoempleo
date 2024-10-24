d6773f58ff35662dcd88847032ba3aadba4e1ebe
Román Ríos <romanrios@live.com>
2024-10-24 02:43:42 -0300

    dynamic import - async goToFlow

--- flows/flowsTercerNivel.js
+++ flows/flowsTercerNivel.js
@@ -1,14 +1,5 @@
 const { addKeyword } = require('@bot-whatsapp/bot');
 
-// const {
-//     flowExpoRosario,
-//     flowExpoSantaFe,
-//     flowImpulsa,
-//     flowPracticasLaborales,
-//     flowEmpleoJoven,
-//     flowOtrasConsultas
-// } = require('./flowsSegundoNivel');
-
 
 // TERCER NIVEL DE RESPUESTA
 
@@ -23,22 +14,36 @@ const flowQuieroRealizarOtraConsulta = addKeyword(['1'])
             '👉 *5.* Prácticas Laborales Formativas',
             '👉 *6.* Otra consulta',
         ],
-        null,
-        null,
-        // [flowExpoRosario, flowExpoSantaFe, flowImpulsa, flowPracticasLaborales, flowEmpleoJoven, flowOtrasConsultas]
+        { capture: true },
+        async (ctx, { gotoFlow }) => {
+            const body = ctx.body;
+            if (body.includes('1')) {
+                const { flowExpoRosario } = require('./flowsSegundoNivel');
+                return gotoFlow(flowExpoRosario);
+            } else if (body.includes('2')) {
+                const { flowExpoSantaFe } = require('./flowsSegundoNivel');
+                return gotoFlow(flowExpoSantaFe);
+            } else if (body.includes('3')) {
+                const { flowImpulsa } = require('./flowsSegundoNivel');
+                return gotoFlow(flowImpulsa);
+            } else if (body.includes('4')) {
+                const { flowEmpleoJoven } = require('./flowsSegundoNivel');
+                return gotoFlow(flowEmpleoJoven);
+            } else if (body.includes('5')) {
+                const { flowPracticasLaborales } = require('./flowsSegundoNivel');
+                return gotoFlow(flowPracticasLaborales);
+            } else if (body.includes('6')) {
+                const { flowOtrasConsultas } = require('./flowsSegundoNivel');
+                return gotoFlow(flowOtrasConsultas);
+            }
+        },
     )
-
 
 const flowGracias = addKeyword(['2'])
     .addAnswer('¡Gracias por comunicarte con la Secretaría de Empleo y Desarrollo Emprendedor del Gobierno de Santa Fe! ¡Por cualquier otra consulta no dudes en escribirme!')
 
-
-
 const flowContactarme = addKeyword(['3'])
     .addAnswer('¡Gracias por comunicarte con la Secretaría de Empleo y Desarrollo Emprendedor del Gobierno de Santa Fe! En breve, nuestro equipo se pondrá en contacto con usted.')
-
-
-
 
 
 module.exports = {
